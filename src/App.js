a1dd12c4bb883e000b65b4cfbe8830f2de22bc31
brisketrecipe <110226184+brisketrecipe@users.noreply.github.com>
2023-11-12 16:41:45 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/home/Home';
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/react'
 import { configureChains, createConfig, WagmiConfig } from 'wagmi'
-import { polygonMumbai} from 'wagmi/chains'
+import { sepolia } from 'wagmi/chains'
 import { infuraProvider } from 'wagmi/providers/infura'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import Web3 from "web3";
@@ -13,12 +13,12 @@ import Web3 from "web3";
 
 function App() {
 
-  const chains = [polygonMumbai]
-const projectId = 'f385bf4e147a499aee6b6c2f17ded944'
+  const chains = [sepolia]
+const projectId = '3cc638f3f1709e91a54012f73e691cc61b990de4'
 
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
 // const { chains, publicClient } = configureChains(
-//   [polygon],
+//   [sepolia],
 //   [({ apiKey: 'tTXdAIDIsIsUyC322Hw-FwS0PPYvO5yw' })],
 // )alchemyProvider
 const wagmiConfig = createConfig({
